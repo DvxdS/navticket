b41c192b24923bad6b2a0909c0f0a18ab6a1e4d2
David Soro <d.soro@alustudent.com>
2024-11-12 18:52:46 +0000

    connection to DB

--- backend/navticket/src/app.module.ts
+++ backend/navticket/src/app.module.ts
@@ -4,18 +4,27 @@ import { Company } from './modules/company/companies/companyEntity';
 import { CompanyModule } from './modules/company/companies/companiesModule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+console.log({
+  url : process.env.SUPABASE_URL,
+  host: process.env.SUPABASE_DB_HOST,
+  port: process.env.SUPABASE_DB_PORT,
+  username: process.env.SUPABASE_DB_USERNAME,
+  password: process.env.SUPABASE_DB_PASSWORD,
+  database: process.env.SUPABASE_DB_NAME,
+});
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.SUPABASE_DB_HOST,
-      port: parseInt(process.env.SUPABASE_DB_PORT, 10),
-      username: process.env.SUPABASE_DB_USERNAME,
-      password: process.env.SUPABASE_DB_PASSWORD,
-      database: process.env.SUPABASE_DB_NAME,
-      entities: [Company],
-      synchronize: true,
+      host: 'aws-0-eu-west-3.pooler.supabase.com',
+      port: 6543,
+      username: 'postgres.xootvwbijtuuyarazfvp',
+      password: '6NMRY0Um8IYmJxXF',
+      database: 'postgres',
+      entities : [Company],
+      synchronize: true
     }),
 
     CompanyModule,
